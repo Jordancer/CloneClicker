521427db6ffc5a81af632efd3eb8cbb111be98b7
Jordan Dancer <112099523+Jordancer@users.noreply.github.com>
2022-09-16 22:57:37 -0400

    Clone Clicker

--- app.js
+++ app.js
@@ -1,8 +1,19 @@
 let naruto = document.getElementById("naruto");
-let cloneText = document.getElementById("Clone");
 let cloneTotal = 0;
+let upgrade = document.getElementById("upgrade");
+let a = 1;
+upgrade.style.display = "none";
+
+upgrade.addEventListener("click", (e) => {
+  a = 2;
+  cloneCounter.innerHTML = "Total Clones: " + cloneTotal;
+});
 
 naruto.addEventListener("click", (e) => {
-  cloneTotal++;
+  cloneTotal = cloneTotal + a;
   cloneCounter.innerHTML = "Total Clones: " + cloneTotal;
+  if (cloneTotal >= 5) {
+    cloneCounter.innerHTML = "Total Clones: " + cloneTotal;
+    upgrade.style.display = "block";
+  }
 });
